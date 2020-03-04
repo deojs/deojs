eb49cee2035f1d9d87f368cba0698118d0359b04
deojs <deojs@outlook.com>
2020-03-04 09:22:16 +0000

    Use lower case language names. Lookups can now be case insensitive

--- src/core/languages/languages.mjs
+++ src/core/languages/languages.mjs
@@ -10,17 +10,19 @@ class Languages {
         this.languages = {};
 
         // Create Object containing all languages so we can refer to them by name
-        this.languages.PowerShell = PowerShellLanguage;
+        this.languages.powershell = PowerShellLanguage;
     }
 
     /**
      * Creates a new instance of a language class and returns it
      *
      * @param {string} langName The name of the language
+     * @returns {object} - The language class
      */
     getLanguage(langName) {
-        if (this.languages[langName] !== undefined) {
-            return this.languages[langName];
+        const lowerName = langName.toLowerCase();
+        if (this.languages[lowerName] !== undefined) {
+            return this.languages[lowerName];
         }
         throw Error(`Language "${langName}" does not exist!`);
     }
