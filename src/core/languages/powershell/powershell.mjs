e5c5fea5bb1d68e03826fc13cb5c670e7f224e49
deojs <deojs@outlook.com>
2020-03-26 17:56:36 +0000

    Split parsing into chunks. Add pretty print function. Eventually there will be visual progress in the UI for parsing function

--- src/core/languages/powershell/powershell.mjs
+++ src/core/languages/powershell/powershell.mjs
@@ -26,9 +26,51 @@ class PowerShellLanguage {
 
         // return inCode;
         const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
-        parser.feed(inCode);
-        console.log(parser.results);
+        const chunkSize = 100; // Characters
+        const numChunks = Math.ceil(inCode.length / chunkSize);
+
+        for (let i = 0; i < numChunks; i++) {
+            console.log(`Parsing ${(i / numChunks) * 100}%`);
+            parser.feed(inCode.slice(i * 100, (i * 100) + 100));
+        }
+        console.log("Parsing complete");
+
+        if (parser.results.length > 0) {
+            return parser.results[0];
+        }
+
         return {};
+    }
+
+    /**
+     * Pretty prints a powershell AST
+     *
+     * @param {object} ast - The AST to pretty print
+     * @returns {string} - Pretty printed code
+     */
+    prettyPrint(ast) {
+        const recurse = function (obj) {
+            if (obj === null || obj === undefined) {
+                return "";
+            }
+            if (typeof obj === "string") {
+                return obj;
+            }
+            if (Array.isArray(obj)) {
+                let out = "";
+                for (let i = 0; i < obj.length; i++) {
+                    out += recurse(obj[i]);
+                }
+                return out;
+            }
+            if (Object.prototype.hasOwnProperty.call(obj, "data")) {
+                return recurse(obj.data);
+            }
+
+            return "";
+        };
+
+        return recurse(ast);
     }
 
     // parse function
